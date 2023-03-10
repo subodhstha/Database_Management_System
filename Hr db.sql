5a3e80af521d9f2655377d105b379f3222487c38
subodhstha <shresthasubodh73@gmail.com>
2023-03-10 20:18:36 +0545

    DBMS

---
+++ Hr db.sql
@@ -0,0 +1,8 @@
+select table_name from tabs;
+select * from employees;
+describe employees;
+select first_name,salary from employees;
+select * FROM employees
+where salary > 10000;
+select first_name,salary from employees
+where salary > 10000;
